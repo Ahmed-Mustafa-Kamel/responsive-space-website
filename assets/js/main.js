b11cd18b9211debf69c48fd32c3362fa516d67a5
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2024-07-27 10:20:51 +0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,12 @@ const swiper = new Swiper('.travel__swiper', {
 
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewport height
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
